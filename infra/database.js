692433ae107f1137fd74636f5f18f6611c566faf
Davi-Guindani <davigpvieira@gmail.com>
2024-11-16 17:30:35 -0300

    removing credentials log

--- infra/database.js
+++ infra/database.js
@@ -15,13 +15,6 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log({
-      host: process.env.POSTGRES_HOST,
-      port: process.env.POSTGRES_PORT,
-      user: process.env.POSTGRES_USER,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-    });
     throw error;
   } finally {
     await client.end();
